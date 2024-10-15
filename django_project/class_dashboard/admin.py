1d5a6498cae3511d99c1be59a8c9724193c8cc90
Bruno Pinto <89224342+brunobpinto@users.noreply.github.com>
2024-10-15 19:39:24 +0000

    Logo and Class Models Created and Admin Configured

--- django_project/class_dashboard/admin.py
+++ django_project/class_dashboard/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Logo, Class
 
 # Register your models here.
+
+class LogoAdmin (admin.ModelAdmin): 
+    list_display = ['type', 'image', 'color'] 
+    ordering = ['type'] 
+    list_filter = ['type']
+
+admin.site.register(Logo, LogoAdmin)
+
+class ClassAdmin (admin.ModelAdmin): 
+    list_display = ['type', 'name', 'active', 'logo'] 
+    ordering = ['type', '-name'] 
+    list_filter = ['active', 'type']
+
+admin.site.register(Class, ClassAdmin)
--- django_project/class_dashboard/models.py
+++ django_project/class_dashboard/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+# Logo Model: ENG4033, ENG1118, ENG4021
+class Logo(models.Model):
+    image = models.ImageField(upload_to='logos/')  # Store all logos in this location
+    type = models.CharField(max_length=100, 
+        choices={  # Database name : Custom name for user
+        'microcontroladores': 'ENG4033 – Projeto de Microcontroladores', 
+        'matlab': 'ENG1118 – Matlab', 
+        'software': 'ENG4021 - Projeto Integrado de Software',
+        } ) 
+    color = models.CharField(max_length=100)  # Store color of logo
+
+    def __str__(self):
+        return self.type
+
+
+# Class Model: ENG4033, ENG1118, ENG4021
+class Class(models.Model): 
+    name = models.CharField(max_length=100) 
+    type = models.CharField(max_length=100, 
+        choices={  # Database name : Custom name for user
+        'microcontroladores': 'ENG4033 – Projeto de Microcontroladores', 
+        'matlab': 'ENG1118 – Matlab', 
+        'software': 'ENG4021 - Projeto Integrado de Software',
+        } ) 
+    
+    active = models.BooleanField(default=True)
+    logo = models.ForeignKey(Logo, on_delete=models.SET_NULL, null=True)
+    notes = models.TextField(blank=True) 
+    
+
+    def __str__(self):
+        return "%s: %s" % (self.type, self.name)
+    
+    class Meta: 
+        verbose_name = "class" 
+        verbose_name_plural = "classes" 
+
